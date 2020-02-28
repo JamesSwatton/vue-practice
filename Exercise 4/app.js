91b9d3124ea48c7ad1774263b9671aa734068e13
James Swatton <jamesswatton@jamess-imac.lan>
2020-02-28 21:58:20 +0000

    add exercise 4

--- Exercise 4/app.js
+++ Exercise 4/app.js
@@ -1,19 +1,32 @@
 new Vue({
-  el: '#exercise',
-  data: {
-      attachHighlight: false,
-      attachRed: false,
-      chosenClass: 'yellow',
-      progressWidth: 0
-  },
-  methods: {
-    startEffect: function() {
-        const v = this;
-        setInterval(() => v.attachHighlight = !v.attachHighlight, 500) 
+    el: '#exercise',
+    data: {
+        effectClasses: {
+            highlight: true,
+            shrink: false
+        },
+        myStyle: {
+            backgroundColor: 'red',
+            width: '200px',
+            height: '100px',
+            border: '1px solid black'
+        },
+        attachRed: false,
+        chosenClass: 'yellow',
+        progressWidth: 0,
+        inputWidth: 50
     },
-    startProgress: function() {
-        const v = this;
-        return setInterval(() => v.progressWidth += 5, 500);
+    methods: {
+        startEffect: function() {
+            const v = this;
+            setInterval(() => {
+                v.effectClasses.highlight = !v.effectClasses.highlight;
+                v.effectClasses.shrink = !v.effectClasses.shrink;
+            },500)
+        },
+        startProgress: function() {
+            const v = this;
+            return setInterval(() => v.progressWidth += 5, 500);
+        }
     }
-  }
 });
