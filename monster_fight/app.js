ded4af068763746e7ed48a4036ce6dd4ebef4aac
JamesSwatton <jamessswatton@gmail.com>
2020-03-02 22:51:50 +0000

    refactor turn logging

--- monster_fight/app.js
+++ monster_fight/app.js
@@ -4,7 +4,8 @@ new Vue({
         game: false,
         playerHealth: 100,
         monsterHealth: 100,
-        fightLog: [],
+        turn: [],
+        turnLog: [],
     },
     watch: {
         playerHealth: function() {
@@ -27,36 +28,47 @@ new Vue({
             this.game = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
-            this.fightLog = [];
-        },
-        logAttack: function(actionString, playerPoints, monsterPoints) {
-            const playerLogString = `PLAYER ${actionString} FOR ${playerPoints}`;
-            const monsterLogString = `MONSTER HITS PLAYER FOR ${monsterPoints}`;
-            const attackLog = [playerLogString, monsterLogString];
-            this.fightLog.push(attackLog);
+            this.turn = [];
+            this.turnLog = [];
         },
         attack: function(damageMin = 3, damageMax = 10) {
-            this.monsterHealth -= this.calculateDamage(damageMin, damageMax); 
+            this.turn = [];
+            const damage = this.calculateDamage(damageMin, damageMax); 
+            this.monsterHealth -= damage;             
             this.monsterAttacks();
-            this.logAttack('HITS MONSTER', playerAttack, monsterAttack);
+            this.turn.push({
+                isPlayer: true,
+                text: 'PLAYER HITS MONSTER FOR ' + damage
+            });
+            this.turnLog.push(this.turn);
         },
         specialAttack: function() {
             this.attack(10, 20)
         },
         heal: function() {
+            this.turn = [];
             if (this.playerHealth <= 90) {
                 this.playerHealth += 10;
             } else {
                 this.playerHealth = 100;
             }
             this.monsterAttacks();
-            this.logAttack('HEALS THEMSELF', playerHeal, monsterAttack);
+            this.turn.push({
+                isPlayer: true,
+                text: 'PLAYER HEALS THEMSELF FOR 10'
+            });
+            this.turnLog.push(this.turn);
         },
         giveUP: function() {
             this.game = false;
         },
         monsterAttacks: function() {
-            this.playerHealth -= this.calculateDamage(5, 12);
+            const damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.turn.push({
+                isPlayer: false,
+                text: 'MONSTER HITS PLAYER FOR ' + damage
+            })
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max + 1), min);
