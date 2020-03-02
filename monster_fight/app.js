6993402e130587fcbb8cc88735c79f371207ddb4
JamesSwatton <jamessswatton@gmail.com>
2020-03-02 19:47:07 +0000

    add attacks and heal

--- monster_fight/app.js
+++ monster_fight/app.js
@@ -4,6 +4,25 @@ new Vue({
         startGame: false,
         playerHealth: 100,
         monsterHealth: 100,
+    },
+    methods: {
+        attack: function() {
+            this.monsterHealth -= 1 + Math.floor(Math.random() * 9);
+            this.playerHealth -= 1 + Math.floor(Math.random() * 9);
+        },
+        specialAttack: function() {
+            this.monsterHealth -= 5 + Math.floor(Math.random() * 9);
+            this.playerHealth -= 1 + Math.floor(Math.random() * 9);
+        },
+        heal: function() {
+            this.playerHealth += 1 + Math.floor(Math.random() * 12);
+            this.playerHealth -= 1 + Math.floor(Math.random() * 9);
+        },
+        giveUp: function() {
+            this.startGame = !this.startGame;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        }
     }
 })
 
